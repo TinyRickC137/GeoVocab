7fdee105f7e2671c885e2bf334dd7a7d3dbd05b2
TinyRickC137 <zhuk.inform.tv@yandex.ru>
2019-01-04 21:10:08 +0300

    Creating table with source data

--- create_source_tables.sql
+++ create_source_tables.sql
@@ -1,1 +1,20 @@
-
+DROP TABLE IF EXISTS united_states_al2_al12;
+CREATE TABLE united_states_al2_al12
+(
+    gid        integer,
+    id         integer,
+    country    varchar(254),
+    name       varchar(254),
+    enname     varchar(254),
+    locname    varchar(254),
+    offname    varchar(254),
+    boundary   varchar(254),
+    adminlevel integer,
+    wikidata   varchar(254),
+    wikimedia  varchar(254),
+    timestamp  varchar(254),
+    note       varchar(254),
+    rpath      varchar(254),
+    iso3166_2  varchar(254),
+    geom       varchar
+);
